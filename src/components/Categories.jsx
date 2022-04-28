8943d026ceb123f9436b0da6cb9205166a6f1252
Ubeydullah Onder <ubeyd03@outloo.com>
2022-04-28 11:07:23 +0300

    categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div``
+const Image = styled.image``
+const Info = styled.div``
+const Tıtle = styled.h1``
+const Button = styled.button``
+
+
+
+const categories = () => {
+  return (
+    <Container>
+
+        <Image/>
+
+
+    </Container>
+  )
+}
+
+export default categories
---
+++ src/components/CategoryItems.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const CategoryItems = () => {
+  return (
+    <div>CategoryItems</div>
+  )
+}
+
+export default CategoryItems

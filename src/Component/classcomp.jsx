4d74bd3004945812ef7ec8f5f80158318cebb6aa
Ubeydullah <ubeyd03@outlook.com>
2022-08-09 11:16:36 +0300

    usestate

--- src/Component/classcomp.jsx
+++ src/Component/classcomp.jsx
@@ -1,13 +1,72 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { useState } from 'react';
 
-export default class Classcomp extends Component {
+
+class Classcomp extends Component {
+
+  
+  
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isvisible : false
+    }
+  }
+
+  isShown=(vis,e)=>{
+    vis = !vis;
+    console.log(this)
+    console.log(vis)
+    }
+
   render() {
+    // destructing
+    const {name,surname,salary,job} = this.props
+    const {isvisible} = this.state
     return (
       <div>
-        <h3>Class component</h3>
+        <div className="card">
+          <div className="card-header d-flex justify-content-between" onClick={this.isShown.bind(this,isvisible)}>
+            <h4 className="card-text">{name} {surname}</h4>
+            <i className="fa-solid fa-caret-down"></i>
+          </div>
+          {
+            isvisible ? 
+          <div className="card-body d-flex justify-content-between">
+            <p className="card-text">{job}</p>
+            <p className='card-text'> {salary}</p>
+          </div> : null
+}
+        </div>
+
+        {/* <h3>Class component</h3>
+        <p>{classtitle}</p>
         <input type="button" value="Send" placeholder='gonder' />
-        <button className='btn btn-primary'>button</button>
+        <button className='btn btn-primary'>button</button> */}
       </div>
     )
   }
 }
+
+Classcomp.propTypes = {
+  name: PropTypes.string.isRequired,
+  surname: PropTypes.string.isRequired,
+  job: PropTypes.string.isRequired,
+  salary: PropTypes.string.isRequired,
+  isvisible:PropTypes.bool.isRequired
+
+}
+
+Classcomp.defaultProps = {
+  name :"Ubeydullah",
+  surname:"Önder",
+  job : "Web Developer",
+  salary : "4500",
+  isvisible : false
+}
+
+
+
+export default Classcomp
